4e4f64847623002b0553ab183d0d3d8ad5b9a2a6
Meharban Singh <meharbansingh10.ms@gmail.com>
2026-02-11 19:51:37 -0600

    More updates

--- server.js
+++ server.js
@@ -2,9 +2,33 @@ const express =  require('express')
 const path = require('path')
 
 const app = express()
-const PORT = 3001
+const PORT = 3002
 
+const users = {}
+// Structure: { username: { notes: [ { id, content, createdAt } ] } }
+
+app.use(express.json())
 app.use(express.static('public'))
+
+app.post('/api/login', (req, res) => {
+    const username = req.body.username
+    let isNewUser = false
+
+    console.log(username)
+
+    if(!users[username]) {
+        users[username] = { notes: [] }
+        isNewUser = true
+        console.log(`New user created ${username}`)
+    }
+
+    res.json({
+        success: true,
+        username,
+        message: isNewUser ? 'New user created' : 'Welcome back'
+    })
+})
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
